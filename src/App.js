8c7cb309f43c0782c4d0656090bee94930cd0593
Ansoumane Kande <kande.ans@outlook.com>
2018-01-25 20:34:32 -0500

    Created the Video-Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import './App.css'
-import SearchBar from './Components/search_bar'
 import YTSearch from 'youtube-api-search'
+import SearchBar from './Components/search_bar'
+import VideoList from './Components/video_list'
+import VideoDetail from './Components/video_detail'
 const API_KEY = 'AIzaSyDclA4nKWjX6anU_J2erH30mwdNY7ysW3M'
 
 class App extends Component {
   constructor(props){
   	super(props)
   	this.state = { videos: [] }
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'beyonce'}, (videos) => {
       //ES6 Condensed syntax because the name of the value being passed is identical to the name of data
       this.setState({videos})
       //The most familiar way to accomplish the same thing:
@@ -20,6 +22,8 @@ class App extends Component {
     return (
       <div className="container">
           <SearchBar />
+          <VideoDetail video={this.state.videos[0]}/>
+          <VideoList videos={this.state.videos}/>
       </div>
     )
   }
---
+++ src/Components/video_list_item.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const VideoListItem = ({video}) => {
+// we can imrprove this syntax by simply retrieving video as props directly like demotrated above
+//  const video = props.videos
+  const imageURL = video.snippet.thumbnails.default.url
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageURL} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default VideoListItem
