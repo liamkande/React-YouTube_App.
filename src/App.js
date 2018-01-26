7c9687ca060ec3c96632ef0c4913554b74c0b011
Ansoumane Kande <kande.ans@outlook.com>
2018-01-25 23:20:49 -0500

    Created selected video callback Function.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ const API_KEY = 'AIzaSyDclA4nKWjX6anU_J2erH30mwdNY7ysW3M'
 class App extends Component {
   constructor(props){
   	super(props)
-  	this.state = { videos: [] }
+  	this.state = {
+      videos: [],
+      selectedVideo: null
+     }
+
     YTSearch({key: API_KEY, term: 'beyonce'}, (videos) => {
       //ES6 Condensed syntax because the name of the value being passed is identical to the name of data
-      this.setState({videos})
+      //this.setState({videos})
       //The most familiar way to accomplish the same thing:
-      //this.setState({videos: videos })
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+       })
     })
   }
 
@@ -22,8 +29,10 @@ class App extends Component {
     return (
       <div className="container">
           <SearchBar />
-          <VideoDetail video={this.state.videos[0]}/>
-          <VideoList videos={this.state.videos}/>
+          <VideoDetail video={this.state.selectedVideo}/>
+          <VideoList
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos={this.state.videos}/>
       </div>
     )
   }
